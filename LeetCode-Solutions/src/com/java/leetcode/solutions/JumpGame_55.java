3a3894afc7c415639c0cd9a61951430fa42bb90d
Karthik CM <karthikcm.77@gmail.com>
2021-07-20 16:52:07 -0700

    LeetCode Solutions commit

--- LeetCode-Solutions/src/com/java/leetcode/solutions/JumpGame_55.java
+++ LeetCode-Solutions/src/com/java/leetcode/solutions/JumpGame_55.java
@@ -3,23 +3,20 @@ package com.java.leetcode.solutions;
 public class JumpGame_55 {
 	
 	public static void main(String[] args) {
-		
+		int nums[] = {2,3,1,1,4};
+		System.out.println(canJump(nums));
 	}
 	
-	public boolean canJump(int[] nums) {
-		int reach = 0;
+	// Intuition : Work backwards 
+	public static boolean canJump(int[] nums) {
+		int lastGoodIndexPosition = nums.length-1;
 		
-        for(int i=0; i<nums.length; i++){
-            if(reach < i)
-            	break;
-            
-            if(reach < i+nums[i]){
-                reach = i+nums[i];
-            }
-            
-            if(reach >= nums.length-1)
-                return true;
-        }
-        return false;
+		for(int i=nums.length-1; i>=0; i--) {
+			if(i + nums[i] >= lastGoodIndexPosition) {
+				lastGoodIndexPosition = i;
+			}
+		}
+		
+		return lastGoodIndexPosition == 0;
 	 }
 }
